b5a99379a51cd9b06352708b1ed332977d62df37
LNVBV2\bogdan <bogdan.barbu.dev@gmail.com>
2025-08-31 17:39:56 +0300

    fixed base path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import ro from '../i18n/ro.json';
 	import en from '../i18n/en.json';
 	import { _setDictionary } from './+layout';
+	import { base } from '$app/paths';
+
 	let { children } = $props();
 
 	import { init, getLocaleFromNavigator, locale, dictionary } from 'svelte-i18n';
@@ -17,7 +19,7 @@
 </script>
 
 <svelte:head>
-	<link href="/prism/prism.css" rel="stylesheet" />
+	<link href="{base}/prism/prism.css" rel="stylesheet" />
 	<!-- <script src="/prism/prism.js"></script> -->
 </svelte:head>
 
